8d0b4ad4edcd2338c1304ad801e89a70ea06d465
Pratik Borole <borolepratik@gmail.com>
2018-06-04 17:40:53 -0700

    Basic Profile
    Basic profile and some minor changes

--- app/src/main/java/com/hackathon156/donteatalone/SignInActivity.kt
+++ app/src/main/java/com/hackathon156/donteatalone/SignInActivity.kt
@@ -1,12 +1,41 @@
 package com.hackathon156.donteatalone
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        buttonSignIn.setOnClickListener { _ ->
+            if (isValidEmail(txtEmailSignIn.text) && isValidPassword(txtPasswordSignIn.text)) {
+                signIn()
+            } else {
+                signInFailed()
+            }
+        }
+    }
+
+    private fun isValidEmail(target: CharSequence): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
+    private fun isValidPassword(target: CharSequence): Boolean {
+        return !TextUtils.isEmpty(target)
+    }
+
+    private fun signIn() {
+        Toast.makeText(this, "Sign in clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun signInFailed() {
+        Toast.makeText(this, "Please check the credentials", Toast.LENGTH_SHORT).show()
     }
 }
